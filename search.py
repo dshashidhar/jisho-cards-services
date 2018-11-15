284bf04350394e95fe78978a028006a163f2db81
Jeffrey Zhang <jzhang11235@gmail.com>
2018-11-15 21:08:56 +1100

    Configure CORS header

--- search.py
+++ search.py
@@ -4,4 +4,6 @@ import requests
 def handler(event, context):
     res = requests.get('https://jisho.org/api/v1/search/words',
                        params=event['queryStringParameters'])
-    return {'statusCode': 200, 'body': res.text}
+    return {'statusCode': 200,
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'body': res.text}
